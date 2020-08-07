42ca65c6ab9be5c1d842aa93ab4f30b7e62713b8
vanshianec <ivaniovov@abv.bg>
2020-08-07 14:38:22 +0300

    Added more security functionality

---
+++ src/main/java/diet/dietgenerator/service/services/implementations/SecurityServiceImpl.java
@@ -0,0 +1,47 @@
+package diet.dietgenerator.service.services.implementations;
+
+import diet.dietgenerator.service.services.SecurityService;
+import diet.dietgenerator.service.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityServiceImpl implements SecurityService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(SecurityServiceImpl.class);
+
+    public SecurityServiceImpl(AuthenticationManager authenticationManager, UserService userService) {
+        this.authenticationManager = authenticationManager;
+        this.userService = userService;
+    }
+
+    @Override
+    public String findLoggedInUsername() {
+        Object userDetails = SecurityContextHolder.getContext().getAuthentication().getDetails();
+        if (userDetails instanceof UserDetails) {
+            return ((UserDetails)userDetails).getUsername();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void autoLogin(String email, String password) {
+        UserDetails userDetails = userService.loadUserByUsername(email);
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+
+        authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+        if (usernamePasswordAuthenticationToken.isAuthenticated()) {
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            logger.debug(String.format("Auto login %s successfully!", email));
+        }
+    }
+}
